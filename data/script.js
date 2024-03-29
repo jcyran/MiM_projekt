e4f42a6bfe97ff7daf1e020bd271aec949fa8983
PandaMisiu <bobjanusz@onet.pl>
2024-03-29 13:59:20 +0100

    strona

---
+++ data/script.js
@@ -0,0 +1,27 @@
+const temperatureValue = document.getElementById("temperature");
+const humidityValue = document.getElementById("humidity");
+const pressureValue = document.getElementById("pressure");
+
+const temperatureIcon = document.getElementById("temperature-icon");
+const humidityIcon = document.getElementById("humidity-icon");
+
+const humidityIconNames = ["humidity_low", "humidity_mid", "humidity_high"];
+const temperatureIconNames = ["ac_unit", "thermometer", "mode_heat"];
+
+const updateValues = function () {
+  const tempV = temperatureValue.textContent;
+  console.log(tempV);
+  console.log(temperatureIcon);
+  if (tempV <= 0) temperatureIcon.textContent = temperatureIconNames[0];
+  else if (tempV > 0 && tempV < 30)
+    temperatureIcon.textContent = temperatureIconNames[1];
+  else if (tempV >= 30) temperatureIcon.textContent = temperatureIconNames[2];
+
+  let humV = humidityValue.textContent;
+  if (humV > 0 && humV < 50) humidityIcon.textContent = humidityIconNames[0];
+  else if (humV >= 50 && humV < 90)
+    humidityIcon.textContent = humidityIconNames[1];
+  else if (humV > 90) humidityIcon.textContent = humidityIconNames[2];
+};
+
+setInterval(updateValues, 1000);
